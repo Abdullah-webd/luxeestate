5da32c4877190b86f0e7ce9559af6ae06a5c3df0
Abdullah-webd <ajalabdllh@gmail.com>
2025-05-07 14:10:24 +0100

    add backend setup

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.listen(3000, () => {
+  console.log('God bless u all pls u can all work on the backend now the frontend is done');
+})
